a7b6dc83754df05d51a024459d95a8ec3dabbb03
John Sabo <saboj@moravian.edu>
2022-02-10 08:57:14 -0500

    Add documentation of params and return values

--- grades.py
+++ grades.py
@@ -2,6 +2,9 @@
 def compute_hw_average(grades):
     """
     Compute the average grade for a list of assignment grades.
+
+    Param: list of grades (all floats)
+    Return: float representing the average grade
     """
     if len(grades) == 0:
         return 0
